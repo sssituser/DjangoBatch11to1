74e01da0a8d6eff1dfed8808f50e227083f6a37f
arun <allamarunkumar@gmail.com>
2025-09-15 13:06:07 +0530

    Project completed

--- stumfp/stuapp/urls.py
+++ stumfp/stuapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from stuapp import views
 
+
 urlpatterns =[
     path('',views.index,name='addStudent'),
     path('students/',views.getStudents,name='stulist'),
-    path('update/',views.editStudent,name='editstudent'),
-    path('show/',views.showStudent,name='showstudent'),
-    path('del/',views.delStudent,name='deletestudent'),
+    path('update/<int:id> ',views.editStudent,name='editstudent'),
+    path('show/<int:id>',views.showStudent,name='showstudent'),
+    path('del/<int:id>',views.delStudent,name='deletestudent'),
     
 ]
+
--- stumfp/stuapp/views.py
+++ stumfp/stuapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from stuapp.forms import StudentForm
 from stuapp.models import Student
 # Create your views here.
@@ -17,11 +17,29 @@ def getStudents(request):
     stu_list_dict = {'stu_list':stu_list}
     return render(request,'stuapp/students.html',stu_list_dict)
 
-def editStudent(request):
-    return render(request,'stuapp/editstu.html')
 
-def showStudent(request):
-   return render(request,'stuapp/showstu.html')
+def editStudent(request, id):
+    stu = get_object_or_404(Student, id=id)   # Fetch student by ID
+  
+    if request.method == 'POST':
+        stu_Form = StudentForm(request.POST, instance=stu)  # Bind form with POST data and existing student
+        if stu_Form.is_valid():
+            stu_Form.save()   # Save updated data
+            return redirect('stulist')
+    else:
+        stu_Form = StudentForm(instance=stu)  # Pre-fill form with existing student data
+    return render(request, 'stuapp/editstu.html', {'stu_Form': stu_Form})
 
-def delStudent(request):
-    return render(request,'stuapp/deleteStudent.html')
+
+
+
+def showStudent(request,id):
+    stu = get_object_or_404(Student,id=id);
+    return render(request,'stuapp/showstu.html',{'stu':stu})
+
+def delStudent(request,id):
+    stu = get_object_or_404(Student,id=id)
+    if request.method == 'POST':
+        stu.delete()
+        return redirect('stulist')
+    return render(request,'stuapp/deleteStudent.html',{'stu':stu})
